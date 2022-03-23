d0df2253a1aa2e09ad370b4d1c42933c57f71c83
Magali Gracia <magali.gracia@neoris.com>
2022-03-23 10:23:26 -0300

    Introduccion

---
+++ 01 - Fundamentos/assets/js/alerts.js
@@ -0,0 +1,6 @@
+alert("Hola mundo");
+let nombre = prompt("Ingrese nombre");
+console.info(nombre); 
+
+const selection =  confirm("Esta seguro que desea ingresar?");
+console.log(selection);
---
+++ 01 - Fundamentos/assets/js/app.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 10;
+let c = 10;
+
+console.log(a);
+console.warn(a);
+console.error(a);
+console.info(a);
+console.table({a, b, c})
